55f4a33312e5280e12dadd0794f01eedf396bcfb
Tatiana Maddix <tatianamaddix@gmail.com>
2022-11-07 18:00:09 -0500

    first push

---
+++ app.js
@@ -0,0 +1,23 @@
+var t = 16;
+var d = 28;
+var a = 23;
+var m = 17;
+var r = 53;
+var e = 27
+var s = 12;
+var l = 6;
+var n = 1;
+var f = 13;
+
+console.log(t+d);
+console.log(a+m);
+console.log(r-e);
+console.log(f-n);
+console.log(t*s);
+console.log(f*l);
+console.log(s/l);
+console.log(d/l);
+console.log(n++);
+console.log(r++);
+console.log(f--);
+console.log(d--);
